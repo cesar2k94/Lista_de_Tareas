d1e8831823636e484485fc77aeac3b631061df71
Julio <juliomederosarias@gmail.com>
2021-02-27 15:55:52 -0300

    Formulario de tareas

--- lista_tareas/src/App.js
+++ lista_tareas/src/App.js
@@ -1,10 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
+const { v4: uuidv4 } = require('uuid');
 
 const App = () => {
+    const [tareas, cambiarTareas]= useState(
+        [
+            {
+                id:uuidv4(),
+                texto: "Lavar",
+                completada: false
+            },
+            {
+                id:uuidv4(),
+                texto: "Fregar",
+                completada: false
+            }
+        ]
+    );
     return (    
         <div className="contenedor">
             <Header /> 
+            <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
         </div>
      );
 }
